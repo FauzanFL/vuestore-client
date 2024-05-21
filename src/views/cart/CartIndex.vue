7c2c61aa66ccffd83217fee62d74646fd5f1f5a7
Fauzan FL <fauzanarth@gmail.com>
2024-05-21 22:20:38 +0700

    initial commit

---
+++ src/views/cart/CartIndex.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div id="page-wrap">
+            <h1>Shopping Cart</h1>
+            <CartItem v-for="item in cartItems" :key="item.id" :item="item" v-on:remove-item="removeFromCart($event)"/>
+            <h3 id="total-price">Total: Rp. {{ totalPrice }}</h3>
+            <button id="checkout-button">Checkout</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CartItem from '../../components/CartItem'
+export default {
+    components: {
+        CartItem
+    },
+    data() {
+        return {
+            cartItems: []
+        }
+    },
+    computed: {
+        totalPrice() {
+            return this.cartItems.reduce((acc, item) => acc + Number(item.price), 0)
+        }
+    },
+    methods: {
+        async removeFromCart(product) {
+            await axios.delete(`http://localhost:3000/api/orders/user/1/product/${product}`)
+            let indexCart = this.cartItems.map(item => item.code).indexOf(product)
+
+            this.cartItems.splice(indexCart, 1)
+        }
+    },
+    async created() {
+        const result = await axios.get('http://localhost:3000/api/orders/user/1')
+        let data = Object.assign({},
+            ...(result.data.map(result => ({ cart_items: result.products })))
+        )
+        this.cartItems = data.cart_items
+    
+    }
+}
+</script>
+
+<style scoped>
+  h1 {
+    border-bottom: 1px solid #41B883;
+    margin: 0;
+    margin-top: 16px;
+    padding: 16px;
+  }
+  #total-price {
+    padding: 16px;
+    text-align: right;
+  }
+  #checkout-button {
+    width: 100%;
+  }
+</style>
